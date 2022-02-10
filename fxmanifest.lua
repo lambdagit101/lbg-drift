9a017a10a3c4cbfaac0c50a9256ae904d2ccf00b
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2022-02-10 16:51:23 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,9 +4,6 @@ game 'gta5'
 author 'lambdaguy101'
 version '1.0.0'
 
-dependencies {
-    '/gameBuild:2189',
-    '/native:0x5AC79C98C5C17F05'
-}
+dependency '/gameBuild:2189'
 
 client_script 'client.lua'
